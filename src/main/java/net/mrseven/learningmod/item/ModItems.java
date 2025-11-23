0001fb7e10b5c424b79769c0459025357cbc0339
V3AI-Seven <127050193+V3AI-Seven@users.noreply.github.com>
2025-11-23 14:08:02 -0500

    Custom item with custom action

--- src/main/java/net/mrseven/learningmod/item/ModItems.java
+++ src/main/java/net/mrseven/learningmod/item/ModItems.java
@@ -2,6 +2,7 @@ package net.mrseven.learningmod.item;
 
 import net.minecraft.world.item.Item;
 import net.mrseven.learningmod.LearningMod;
+import net.mrseven.learningmod.item.custom.ChiselItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -9,8 +10,14 @@ import net.neoforged.neoforge.registries.DeferredRegister;
 public class ModItems {
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(LearningMod.MOD_ID);
 
-    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> BISMUTH = ITEMS.register("bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth",
+            () -> new Item(new Item.Properties()));
+
+    public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
+            () -> new ChiselItem(new Item.Properties().durability(32)));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
