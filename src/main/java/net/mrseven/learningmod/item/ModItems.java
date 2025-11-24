b48b5d3a2c23ac5391aef0023fdba40a97061afd
V3AI-Seven <127050193+V3AI-Seven@users.noreply.github.com>
2025-11-23 20:51:37 -0500

    Custom food, and two methods of making fuel

--- src/main/java/net/mrseven/learningmod/item/ModItems.java
+++ src/main/java/net/mrseven/learningmod/item/ModItems.java
@@ -3,6 +3,7 @@ package net.mrseven.learningmod.item;
 import net.minecraft.world.item.Item;
 import net.mrseven.learningmod.LearningMod;
 import net.mrseven.learningmod.item.custom.ChiselItem;
+import net.mrseven.learningmod.item.custom.FuelItem;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -18,6 +19,15 @@ public class ModItems {
 
     public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel",
             () -> new ChiselItem(new Item.Properties().durability(32)));
+
+    public static final DeferredItem<Item> RADISH = ITEMS.register("radish",
+            () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+
+    public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
+            () -> new FuelItem(new Item.Properties(), 800));
+
+    public static final DeferredItem<Item> STARLIGHT_ASHES = ITEMS.register("starlight_ashes",
+            () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
