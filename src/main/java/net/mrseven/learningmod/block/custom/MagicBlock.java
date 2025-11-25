f5441647ce8bae59d2ca03597c6f199e278cda67
V3AI-Seven <127050193+V3AI-Seven@users.noreply.github.com>
2025-11-24 19:52:21 -0500

    Custom tag for the magic block

--- src/main/java/net/mrseven/learningmod/block/custom/MagicBlock.java
+++ src/main/java/net/mrseven/learningmod/block/custom/MagicBlock.java
@@ -17,6 +17,7 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.mrseven.learningmod.item.ModItems;
+import net.mrseven.learningmod.util.ModTags;
 
 import java.util.List;
 
@@ -35,12 +36,16 @@ public class MagicBlock extends Block {
     @Override
     public void stepOn(Level level, BlockPos pos, BlockState state, Entity entity) {
         if(entity instanceof ItemEntity itemEntity){
-            if(itemEntity.getItem().getItem() == ModItems.RAW_BISMUTH.get()) {
+            if(isValidItem(itemEntity.getItem())) {
                 itemEntity.setItem(new ItemStack(Items.DIAMOND, itemEntity.getItem().getCount()));
             }
         }
 
         super.stepOn(level, pos, state, entity);
+    }
+
+    private boolean isValidItem(ItemStack item) {
+        return item.is(ModTags.Items.TRANSFORMABLE_ITEMS);
     }
 
     @Override
